d722ad97d94d81b0d00a991211a69b00f9cb5a85
unknown <j428cbtv@duck.com>
2024-07-31 17:58:07 +0300

    update ClassScheduleTableSeeder

--- database/seeders/ClassScheduleTableSeeder.php
+++ database/seeders/ClassScheduleTableSeeder.php
@@ -10,10 +10,10 @@ class ClassScheduleTableSeeder extends Seeder
     public function run()  
     {  
         $classSchedules = [  
-            ['ClassID' => 1, 'CourseID' => 1, 'TimeSlot' => '09:00:00'],  
-            ['ClassID' => 1, 'CourseID' => 2, 'TimeSlot' => '11:00:00'],  
-            ['ClassID' => 2, 'CourseID' => 3, 'TimeSlot' => '10:00:00'],  
-            ['ClassID' => 3, 'CourseID' => 4, 'TimeSlot' => '14:00:00'],  
+            ['DayofWeek' => 'Monday', 'ClassID' => 1, 'CourseID' => 1, 'InstructorID' => 1, 'TimeSlot' => '09:00:00'],  
+            ['DayofWeek' => 'Wednesday', 'ClassID' => 1, 'CourseID' => 2, 'InstructorID' => 2, 'TimeSlot' => '11:00:00'],  
+            ['DayofWeek' => 'Friday', 'ClassID' => 2, 'CourseID' => 3, 'InstructorID' => 1, 'TimeSlot' => '10:00:00'],  
+            ['DayofWeek' => 'Tuesday', 'ClassID' => 3, 'CourseID' => 4, 'InstructorID' => 3, 'TimeSlot' => '14:00:00'],  
         ];  
 
         DB::table('class_schedules')->insert($classSchedules);  
